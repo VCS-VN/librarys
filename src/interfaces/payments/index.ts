c2b176a994b96686956dacbd3c32a4a8d30f582d
Thai-bug <hxt12021999@gmail.com>
2023-09-25 00:21:54 +0700

    It was the best of times, it was the worst of times

--- src/interfaces/payments/index.ts
+++ src/interfaces/payments/index.ts
@@ -1,7 +1,17 @@
+import { Status } from '@/model';
 import { Observable } from 'rxjs';
 
 export interface IPaymentService {
   add(payment: IPayment): Observable<IPayment>;
 }
 
-export interface IPayment {}
+export interface IPayment {
+  id?: string;
+  userId: string;
+  amount: number;
+  countryId: number;
+  description?: string;
+  descriptor?: string;
+  status: Status;
+  resource: string;
+}
