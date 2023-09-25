4ad79d2195988910a5aa014219a33f88f852430a
Thai-bug <hxt12021999@gmail.com>
2023-09-25 15:48:12 +0700

    Last time I said it works? I was kidding.  Try this.

--- src/transaction-log/transaction-logger.service.ts
+++ src/transaction-log/transaction-logger.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import dayjs from 'dayjs';
+import * as dayjs from 'dayjs';
 import { TransactionLogger } from './schemas/transaction-logger.schema';
 
 type CreateLoggerRequest = {
