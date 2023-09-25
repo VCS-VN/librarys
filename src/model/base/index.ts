c0430dff8de16fc1a362c4608d69630504a51a0e
Thai-bug <hxt12021999@gmail.com>
2023-09-25 21:20:45 +0700

    I know, I know, this is not how I’m supposed to do it, but I can't think of something better.

--- src/model/base/index.ts
+++ src/model/base/index.ts
@@ -8,6 +8,21 @@ import {
 } from 'typeorm';
 import { ulid } from 'ulid';
 
+export abstract class AbstractEntity {
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'timestamptz',
+  })
+  createdAt?: Date;
+
+  @UpdateDateColumn({
+    name: 'updated_at',
+    type: 'timestamptz',
+  })
+  updatedAt?: Date;
+}
+
+
 export abstract class AbstractEntityV2 extends BaseEntity {
   @CreateDateColumn({
     name: 'created_at',
