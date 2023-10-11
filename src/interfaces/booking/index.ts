1351a50bcd21f77ad67f15efdb3115c7e676a4cc
Thai-bug <hxt12021999@gmail.com>
2023-10-11 15:42:00 +0700

    I cannot believe that it took this long to write a test for this.

--- src/interfaces/booking/index.ts
+++ src/interfaces/booking/index.ts
@@ -2,7 +2,7 @@ import { PAYMENT_RESOURCE, PAYMENT_TYPE, Status } from '@/model';
 import { Observable } from 'rxjs';
 
 export interface IBookingService {
-  add(payment: IBooking): Observable<IBooking>;
+  create(payment: IBooking): Observable<IBooking>;
   // getByPaymentId(payment: IBooking): Observable<IBooking>;
   // updatePayment(payment: IBooking): Observable<IBooking>;
 }
