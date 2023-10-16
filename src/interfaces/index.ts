a964dcd81de5a048673371782a4bea8b0d78539f
Thai-bug <hxt12021999@gmail.com>
2023-10-16 19:03:32 +0700

    these guys are flipped

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,7 +1,9 @@
 export * from './payments';
 
-export * from './notification'
+export * from './notification';
 
-export * from './booking'
+export * from './booking';
+
+export * from './user';
 
 export interface IEmpty {}
