4cafbffd6ed208389154f535abacd7973f059008
Thai-bug <hxt12021999@gmail.com>
2023-10-21 20:24:45 +0700

    GIT :/

--- src/grpc-connect/port.ts
+++ src/grpc-connect/port.ts
@@ -9,3 +9,5 @@ export const USER_PORT = process.env.USER_PORT || 9000;
 export const AUTH_PORT = process.env.AUTH_PORT || 9000;
 
 export const CUSTOMER_PORT = process.env.CUSTOMER_PORT || 9000;
+
+export const SALES_PORT = process.env.SALES_PORT || 9000;
