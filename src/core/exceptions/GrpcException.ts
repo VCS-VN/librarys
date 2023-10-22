54e0e61f132e9ef01847ca005e8ec0a979a41eaf
Thai-bug <hxt12021999@gmail.com>
2023-10-23 01:00:14 +0700

    Committed some changes

--- src/core/exceptions/GrpcException.ts
+++ src/core/exceptions/GrpcException.ts
@@ -1,12 +1,8 @@
 import {
   ArgumentsHost,
-  BadRequestException,
   Catch,
   ExceptionFilter,
   HttpStatus,
-  InternalServerErrorException,
-  NotFoundException,
-  UnauthorizedException,
 } from '@nestjs/common';
 import { RpcException } from '@nestjs/microservices';
 import { Response } from 'express';
@@ -20,53 +16,46 @@ export class RpcExceptionFilter implements ExceptionFilter {
 
     switch (error.code) {
       case 1:
-        throw new BadRequestException(error.details);
-        // return response.status(HttpStatus.EXPECTATION_FAILED).json({
-        //   message: error.details,
-        //   statusCode: HttpStatus.BAD_REQUEST,
-        // });
+        return response.status(HttpStatus.BAD_REQUEST).json({
+          message: error.details,
+          statusCode: HttpStatus.BAD_REQUEST,
+        });
 
       case 2:
-        throw new BadRequestException(error.details);
-        // return response.status(HttpStatus.EXPECTATION_FAILED).json({
-        //   message: error.details,
-        //   statusCode: HttpStatus.EXPECTATION_FAILED,
-        // });
+        return response.status(HttpStatus.BAD_REQUEST).json({
+          message: error.details,
+          statusCode: HttpStatus.BAD_REQUEST,
+        });
 
       case 3:
-        throw new BadRequestException(error.details);
-        // return response.status(HttpStatus.BAD_REQUEST).json({
-        //   message: error.details,
-        //   statusCode: HttpStatus.BAD_REQUEST,
-        // });
+        return response.status(HttpStatus.BAD_REQUEST).json({
+          message: error.details,
+          statusCode: HttpStatus.BAD_REQUEST,
+        });
 
       case 5:
-        throw new NotFoundException(error.details);
-        // return response.status(HttpStatus.NOT_FOUND).json({
-        //   message: error.details,
-        //   statusCode: HttpStatus.NOT_FOUND,
-        // });
+        return response.status(HttpStatus.NOT_FOUND).json({
+          message: error.details,
+          statusCode: HttpStatus.NOT_FOUND,
+        });
 
       case 10:
-        throw new InternalServerErrorException(error.details);
-        // return response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
-        //   message: error.details,
-        //   statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
-        // });
+        return response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+          message: error.details,
+          statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        });
 
       case 13:
-        throw new InternalServerErrorException(error.details);
-        // return response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
-        //   message: error.details,
-        //   statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
-        // });
+        return response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+          message: error.details,
+          statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        });
 
       case 16:
-        throw new UnauthorizedException(error.details);
-        // return response.status(HttpStatus.UNAUTHORIZED).json({
-        //   message: error.details,
-        //   statusCode: HttpStatus.UNAUTHORIZED,
-        // });
+        return response.status(HttpStatus.UNAUTHORIZED).json({
+          message: error.details,
+          statusCode: HttpStatus.UNAUTHORIZED,
+        });
     }
 
     response.json(error);
