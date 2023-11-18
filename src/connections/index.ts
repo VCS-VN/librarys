f7d277fe831720245a8532c59f9ad6f40ccb3e18
Thai-bug <hxt12021999@gmail.com>
2023-11-18 23:32:36 +0700

    wip

--- src/connections/index.ts
+++ src/connections/index.ts
@@ -1,1 +1,3 @@
 export * from './port';
+
+export * from './topic';
---
+++ src/connections/topic.ts
@@ -0,0 +1,3 @@
+export const ORDER_TOPIC = 'amp.service.order.topic';
+
+export const PAYMENT_TOPIC = 'amp.service.payment.topic';
