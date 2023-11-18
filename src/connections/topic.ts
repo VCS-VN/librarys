dc223543fd90a8dc7600fb787f9c7f1cb7c7377d
Thai-bug <hxt12021999@gmail.com>
2023-11-19 00:13:06 +0700

    Added a banner to the default admin page. Please have mercy on me =(

--- src/connections/topic.ts
+++ src/connections/topic.ts
@@ -1,3 +1,11 @@
 export const ORDER_TOPIC = 'amp.service.order.topic';
 
 export const PAYMENT_TOPIC = 'amp.service.payment.topic';
+
+export const STORE_TOPIC = 'amp.service.store.topic';
+
+export const PRODUCT_TOPIC = 'amp.service.product.topic';
+
+export const CATEGORY_TOPIC = 'amp.service.category.topic';
+
+export const USER_TOPIC = 'amp.service.user.topic';
