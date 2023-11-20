9ce7908242342d64cfc9beb2a9fc4e7e351f83b9
Thai-bug <hxt12021999@gmail.com>
2023-11-20 21:02:49 +0700

    starting the service is always better

--- src/model/stripe/index.ts
+++ src/model/stripe/index.ts
@@ -3,3 +3,5 @@ export * from './subscription';
 export * from './account';
 
 export * from './location';
+
+export * from './plan';
---
+++ src/model/stripe/plan.ts
@@ -0,0 +1,8 @@
+import { STATUS } from '@/enum';
+
+export interface IStripePlan {
+  id?: string;
+  priceId?: string;
+  productId?: string;
+  status?: STATUS;
+}
