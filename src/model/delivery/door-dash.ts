fa2b8234d6fc6fd9e441d2c4a793cf09d200ff2b
Thai-bug <hxt12021999@gmail.com>
2023-11-25 03:41:38 +0700

    tl;dr

--- src/model/delivery/door-dash.ts
+++ src/model/delivery/door-dash.ts
@@ -1,15 +1,40 @@
-export interface IDoorDashBusiness {
+export interface ICreateBusiness {
+  externalBusinessId: string;
   name: string;
   description: string;
-  externalBusinessId: string;
-  activationStatus: string;
-  createdAt: Date;
-  lastUpdatedAt: string;
-  isTest: boolean;
-  externalMetadata: {
-    numberOfStores: string;
-    clientEmail: string;
-    clientPhoneMumber: string;
-    externalStoreIds: string[];
+  activationStatus?: string;
+}
+export interface IDoorDashBusiness {
+  name?: string;
+  description?: string;
+  externalBusinessId?: string;
+  activationStatus?: string;
+  createdAt?: Date;
+  lastUpdatedAt?: string;
+  isTest?: boolean;
+  externalMetadata?: {
+    numberOfStores?: string;
+    clientEmail?: string;
+    clientPhoneNumber?: string;
+    externalStoreIds?: string[];
   };
+}
+
+export interface ICreateDoorDashStore {
+  externalStoreId: string;
+  name: string;
+  phone_number: string;
+  address: string;
+}
+
+export interface IDoorDashStore {
+  name?: string;
+  externalBusinessId?: string;
+  externalStoreId?: string;
+  phoneNumber?: string;
+  address?: string;
+  status?: string;
+  is_test?: string;
+  created_at?: Date;
+  last_updated_at?: Date;
 }
