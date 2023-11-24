d1f4818cba93f50dface026c3ef8e38aaa43bb07
Thai-bug <hxt12021999@gmail.com>
2023-11-24 16:14:37 +0700

    Arrrrgggg

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,7 @@ export * from './interfaces';
 
 export * from './model';
 
-export * from './request-log';
-
 export * from './enum';
-
-export * from './transaction-log';
 
 export * from './core';
 
