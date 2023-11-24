125aca4c81c0b413fb20cc71314dd9827587d1bf
Thai-bug <hxt12021999@gmail.com>
2023-11-24 15:25:22 +0700

    Hide those navs, boi!

--- src/model/payments/index.ts
+++ src/model/payments/index.ts
@@ -4,6 +4,28 @@ export * from './air-wallex.wh';
 
 export * from './stripe';
 
+export interface IPayment {
+  id?: string;
+  userId?: string;
+  amount?: number;
+  countryId?: number;
+  description?: string;
+  descriptor?: string;
+  statusId?: STATUS;
+  resource?: PAYMENT_RESOURCE;
+  provider?: string;
+  type?: string;
+  currency?: string;
+  paymentType?: PAYMENT_TYPE;
+  paymentId?: string;
+  addedMoney?: boolean;
+  fees?: number;
+  net?: number;
+  storeSlug?: string;
+  intentId?: string;
+  clientSecret?: string;
+}
+
 export interface ICreatePayment {
   id?: string;
   userId?: string;
@@ -21,6 +43,4 @@ export interface ICreatePayment {
   addedMoney?: boolean;
   fees?: number;
   net?: number;
-  intentId?: string;
-  clientSecret?: string;
 }
