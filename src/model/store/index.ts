de649e0d20ea01e47c64d28a098da7bb4d537fe1
Thai-bug <hxt12021999@gmail.com>
2023-11-27 23:34:56 +0700

    Popping stash

--- src/model/store/index.ts
+++ src/model/store/index.ts
@@ -45,28 +45,9 @@ export interface IStore {
   priceId?: string;
   readerId?: string;
   businessType?: string;
+  phoneNumber?: string;
 }
 
-export interface IUpdateStore extends IStore {
-  id?: string;
-  name?: string;
-  paymentMetadata?: IStorePaymentMetadata;
-  deliveryMetadata?: IStoreDeliveryMetadata;
-  slug?: string;
-  subscriptionId?: string;
-  subscription?: StripeSubscription;
-  statusId?: STATUS;
-  email?: string;
-  locationId?: string;
-  placeId?: string;
-  userId?: string;
-  address?: string;
-  baseURL?: string;
-  lat?: number;
-  long?: number;
-  orderTypes: { name: string; code: string }[];
-  priceId?: string;
-  readerId?: string;
-}
+export interface IUpdateStore extends IStore {}
 
 export interface ICreateStore extends IStore {}
