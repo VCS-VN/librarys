c6bada9db262dd2b0b10e167f5ace1e8a618a26c
Thai-bug <hxt12021999@gmail.com>
2024-02-21 12:38:09 +0700

    workaround for ant being a pile of fail

--- src/utils/dayjs/index.ts
+++ src/utils/dayjs/index.ts
@@ -1,20 +1,20 @@
-import * as dayjs from 'dayjs';
-import * as isLeapYear from 'dayjs/plugin/isLeapYear';
-import * as localeData from 'dayjs/plugin/localeData';
-import * as utc from 'dayjs/plugin/utc';
-import * as timezone from 'dayjs/plugin/timezone';
-import weekday from 'dayjs/plugin/weekday';
-import isoWeek from 'dayjs/plugin/isoWeek';
+// import * as dayjs from 'dayjs';
+// import * as isLeapYear from 'dayjs/plugin/isLeapYear';
+// import * as localeData from 'dayjs/plugin/localeData';
+// import * as utc from 'dayjs/plugin/utc';
+// import * as timezone from 'dayjs/plugin/timezone';
+// import weekday from 'dayjs/plugin/weekday';
+// import isoWeek from 'dayjs/plugin/isoWeek';
 
-dayjs().format();
+// dayjs().format();
 
-dayjs.extend(localeData);
-dayjs.extend(isLeapYear);
-dayjs.extend(utc);
-dayjs.extend(timezone);
-dayjs.extend(weekday);
-dayjs.extend(isoWeek);
+// dayjs.extend(localeData);
+// dayjs.extend(isLeapYear);
+// dayjs.extend(utc);
+// dayjs.extend(timezone);
+// dayjs.extend(weekday);
+// dayjs.extend(isoWeek);
 
-dayjs.tz.setDefault('Etc/UCT');
+// dayjs.tz.setDefault('Etc/UCT');
 
-export const instance = dayjs;
+// export const instance = dayjs;
