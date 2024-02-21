5fe930dc5bdfb7fe24cc8c4b9cfe519fc652615c
Thai-bug <hxt12021999@gmail.com>
2024-02-21 12:42:28 +0700

    Deleted API file

--- src/utils/dayjs/index.ts
+++ src/utils/dayjs/index.ts
@@ -1,20 +1,18 @@
-// import * as dayjs from 'dayjs';
-// import * as isLeapYear from 'dayjs/plugin/isLeapYear';
-// import * as localeData from 'dayjs/plugin/localeData';
-// import * as utc from 'dayjs/plugin/utc';
-// import * as timezone from 'dayjs/plugin/timezone';
-// import weekday from 'dayjs/plugin/weekday';
-// import isoWeek from 'dayjs/plugin/isoWeek';
+import dayjs from 'dayjs';
+import isLeapYear from 'dayjs/plugin/isLeapYear';
+import localeData from 'dayjs/plugin/localeData';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+import weekday from 'dayjs/plugin/weekday';
+import isoWeek from 'dayjs/plugin/isoWeek';
 
-// dayjs().format();
+dayjs.extend(localeData);
+dayjs.extend(isLeapYear);
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.extend(weekday);
+dayjs.extend(isoWeek);
 
-// dayjs.extend(localeData);
-// dayjs.extend(isLeapYear);
-// dayjs.extend(utc);
-// dayjs.extend(timezone);
-// dayjs.extend(weekday);
-// dayjs.extend(isoWeek);
+dayjs.tz.setDefault('Etc/UCT');
 
-// dayjs.tz.setDefault('Etc/UCT');
-
-// export const instance = dayjs;
+export default dayjs;
