bc30d9de3d5b7a710d7e75aa6901f516eaf6733f
Thai-bug <hxt12021999@gmail.com>
2024-02-21 10:46:18 +0700

    Updated framework to the lattest version

--- src/model/base/index.ts
+++ src/model/base/index.ts
@@ -1,21 +1,7 @@
-import { BaseEntity, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { CreateDateColumn, UpdateDateColumn } from 'typeorm';
 import { ulid } from 'ulid';
 
 export abstract class AbstractEntity {
-  @CreateDateColumn({
-    name: 'created_at',
-    type: 'timestamptz',
-  })
-  createdAt?: Date;
-
-  @UpdateDateColumn({
-    name: 'updated_at',
-    type: 'timestamptz',
-  })
-  updatedAt?: Date;
-}
-
-export abstract class AbstractEntityV2 extends BaseEntity {
   @CreateDateColumn({
     name: 'created_at',
     type: 'timestamptz',
