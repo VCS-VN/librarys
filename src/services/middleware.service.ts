1bced2e7beab658bf4d8c823c673eaed3c9def89
null <null>
2024-07-09 01:17:49 +0700

    buenas those-things.

--- src/services/middleware.service.ts
+++ src/services/middleware.service.ts
@@ -1,11 +1,11 @@
 import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
-import { NextFunction } from 'express';
+import { NextFunction, Request } from 'express';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   private readonly logger = new Logger();
   use(req: Request, res: Response, next: NextFunction) {
-    this.logger.verbose(`${req.url}`);
+    this.logger.verbose(`${req.baseUrl}`);
 
     next();
   }
