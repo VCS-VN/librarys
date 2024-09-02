5ed62d6624067d69fc56cff5e1d4b214bf169030
null <null>
2024-09-02 23:14:25 +0700

    I was told to leave it alone, but I have this thing called OCD, you see

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,37 +1,19 @@
 export * from './base';
-
 export * from './customer';
-
 export * from './order';
-
 export * from './stripe';
-
 export * from './finances';
-
 export * from './messages';
-
 export * from './order';
-
 export * from './store';
-
 export * from './category';
-
 export * from './product';
-
 export * from './log';
-
 export * from './delivery';
-
 export * from './user';
-
 export * from './notification';
-
 export * from './booking';
-
 export * from './auth';
-
 export * from './assets';
-
 export * from './setting';
-
 export * from './withdraw';
