323ce482b64f40119c6019c355b3f4a03062fd5e
thai-bug <hxt12021999@gmail.com>
2024-11-13 09:55:12 +0700

    Well, it's doing something.

--- src/services/epis.logger.ts
+++ src/services/epis.logger.ts
@@ -20,7 +20,27 @@ export class EPISLogger extends ConsoleLogger {
 
   override log(message: any, context?: string): void {
     if (!EPISLogger.contextsToIgnore.includes(context)) {
+      if (typeof message === 'object') {
+        message = JSON.stringify(message, null, 0);
+      }
+
       super.log(message, context);
     }
+  }
+
+  override error(message: any, trace?: string, context?: string): void {
+    if (typeof message === 'object') {
+      // Minify JSON
+      message = JSON.stringify(message, null, 0);
+    }
+    super.error(message, trace, context);
+  }
+
+  override warn(message: any, context?: string): void {
+    if (typeof message === 'object') {
+      // Minify JSON
+      message = JSON.stringify(message, null, 0);
+    }
+    super.warn(message, context);
   }
 }
