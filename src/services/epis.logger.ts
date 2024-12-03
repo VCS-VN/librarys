89b36ce7124f714260de42ff0e42729a051f5c05
thai-bug <hxt12021999@gmail.com>
2024-12-03 23:24:26 +0700

    Fucking templates.

--- src/services/epis.logger.ts
+++ src/services/epis.logger.ts
@@ -2,19 +2,15 @@ import { ConsoleLogger } from '@nestjs/common';
 
 export class EPISLogger extends ConsoleLogger {
   #appName = 'Company App';
-  private service: string;
-
-  constructor(service: string) {
-    super();
-    this.service = service;
-  }
 
   public setAppName(name: string): void {
     this.#appName = name;
   }
 
   override formatPid(pid: number): string {
-    return `[${this.service}] [${this.#appName}] ${pid}  - `;
+    const service = process.env.APP_NAME;
+
+    return `[${service}] [${this.#appName}] ${pid}  - `;
   }
 
   static contextsToIgnore = [
