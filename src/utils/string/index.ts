30347834e7a4f3da683e5aa8575971785ab19b93
thai-bug <hxt12021999@gmail.com>
2025-02-05 19:50:51 +0700

    More ignore

--- src/utils/string/index.ts
+++ src/utils/string/index.ts
@@ -25,3 +25,7 @@ export function isValidUUID(uuid) {
     /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
   return uuidPattern.test(uuid);
 }
+
+export function formatPhoneNumber(phone: string): string {
+  return phone.replace(/(\+\d{1,3})0/, '$1');
+}
