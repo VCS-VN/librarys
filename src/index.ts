6ac29a2e15f9e8535a597bab307484782baeb7e8
thai-bug <hxt12021999@gmail.com>
2025-02-16 22:43:37 +0700

    Major fixup.

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,8 @@ import 'dotenv/config';
 export * from './connections';
 
 export * from './model';
-
 export * from './enum';
-
 export * from './core';
-
 export * from './utils';
-
 export * from './services';
-
 export * from './controllers';
