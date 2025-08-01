8b180939819ed7271f11e0d34c23dfd83c716456
thai-bug <hxt12021999@gmail.com>
2025-08-02 06:48:41 +0700

    extra debug for stuff module

--- src/helper/address.ts
+++ src/helper/address.ts
@@ -1,7 +1,8 @@
 export const extractAddressComponent = (
   addressComponents: any[],
   type: string,
+  field = 'long_name',
 ) => {
   const comp = addressComponents.find((c) => c.types.includes(type));
-  return comp ? comp.long_name : null;
+  return comp ? comp[field] : null;
 };
